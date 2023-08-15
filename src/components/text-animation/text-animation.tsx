24c5d7715a85644364228d4a0c43e59b1b27f152
Mike Gulline <mike@mikegulline.com>
2023-08-15 16:25:02 -0700

    touchup text animation component

--- src/components/text-animation/text-animation.tsx
+++ src/components/text-animation/text-animation.tsx
@@ -1,26 +1,41 @@
+'use client';
+import { useRef, useEffect } from 'react';
 type TextAnimationProps = {
   text: string;
 };
 export default function TextAnimation({ text }: TextAnimationProps) {
   let count = 0;
+
   const newText = text.split(' ').map((word, i) => (
     <span key={`${word}-${i}`} className='word'>
       {word.split('').map((letter, x) => {
         count++;
-        return (
-          <span
-            key={`${letter}-${x}`}
-            data-wait={count}
-            className='letter'
-            style={{ ['--index']: count }}
-          >
-            {letter}
-          </span>
-        );
+        return <Letter key={`${letter}-${x}`} letter={letter} count={count} />;
       })}
       <span className='letter'>&nbsp;</span>
     </span>
   ));
 
   return <div>{newText}</div>;
+}
+
+type LetterProps = {
+  letter: string;
+  count: number;
+};
+
+function Letter({ letter, count }: LetterProps) {
+  const ref = useRef<HTMLSpanElement | null>(null);
+
+  useEffect(() => {
+    if (ref?.current) {
+      ref.current.style.setProperty('--index', count.toString());
+    }
+  }, [ref, count]);
+
+  return (
+    <span className='letter' ref={ref}>
+      {letter}
+    </span>
+  );
 }
