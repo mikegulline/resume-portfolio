d144cb0d200e946584641898418deeb615d907c8
Mike Gulline <mike@mikegulline.com>
2023-08-16 20:51:42 -0700

    added a burst

--- src/components/wrapper/wrapper.tsx
+++ src/components/wrapper/wrapper.tsx
@@ -8,12 +8,12 @@ export default function Wrapper({ children, full, small }: WrapperProps) {
   if (full) return <div className='px-5 lg:px-10'>{children}</div>;
   if (small)
     return (
-      <div className='container lg:max-w-screen-lg px-5 lg:px-10'>
+      <div className='relative container lg:max-w-screen-lg px-5 lg:px-10'>
         {children}
       </div>
     );
   return (
-    <div className='container xl:max-w-screen-xl m-auto px-5 lg:px-10'>
+    <div className='relative container xl:max-w-screen-xl m-auto px-5 lg:px-10'>
       {children}
     </div>
   );
